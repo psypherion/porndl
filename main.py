ed83fd5c497e48a14783d0f037ccae5e3dc229b1
Sayan Sarkar <72155617+ky13-troj@users.noreply.github.com>
2024-08-10 23:32:03 +0530

    V 0.03
    added playlist downloading support

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from spnkbng import VideoDownloader
+from spnkbng import VideoDownloader, PlaylistDownloader
 
 def main():
-    link = input("Enter the link: ")
-    downloader = VideoDownloader(link)
+    link = input("Enter the URL: ")
+    
+    if "playlist" in link:
+        print("Detected a playlist URL.")
+        downloader = PlaylistDownloader(link)
+    else:
+        print("Detected a single video URL.")
+        downloader = VideoDownloader(link)
+    
     downloader.run()
 
 if __name__ == "__main__":
